6b8df171d90c987263a24123f9741b386df2c6a7
systemspool <jose.anaya1989@gmail.com>
2024-07-11 22:22:54 -0600

    se empieza a separar componentes de la factura

---
+++ src/app/components/factura-view/factura-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factura-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './factura-view.component.html',
+  styleUrl: './factura-view.component.css'
+})
+export class FacturaViewComponent {
+
+}
--- src/app/components/factura/factura.component.ts
+++ src/app/components/factura/factura.component.ts
@@ -1,11 +1,16 @@
 import { Component, inject, Inject, OnInit } from '@angular/core';
 import { FacturaService } from '../../services/factura.service';
 import { Factura } from '../../models/factura';
+import { FacturaViewComponent } from '../factura-view/factura-view.component';
+import { ClientViewComponent } from '../client-view/client-view.component';
+import { CompanyViewComponent } from '../company-view/company-view.component';
+import { ListItemsComponent } from '../list-items/list-items.component';
+import { RowItemComponent } from '../row-item/row-item.component';
 
 @Component({
   selector: 'app-factura',
   standalone: true,
-  imports: [],
+  imports: [FacturaViewComponent, ClientViewComponent,CompanyViewComponent,ListItemsComponent,RowItemComponent],
   templateUrl: './factura.component.html',
   styleUrl: './factura.component.css'
 })
